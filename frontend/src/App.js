13dfd8ca7176e7a48b3646cf593dd3abce5481a5
PRASANTH M <prasanth.krm2003@gmail.com>
2025-03-30 19:54:41 +0530

    feat:add pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,25 @@
+
 import React from 'react'
+import './App.css';
+import {Box} from '@mui/material';
+import {BrowserRouter, Route, Router, Routes} from 'react-router-dom';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './Components/Navbar';
+
 
 export const App = () => {
   return (
-    <div>App</div>
+    // eslint-disable-next-line react/jsx-no-undef
+    
+    <BrowserRouter>
+    <Box width="400px">
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/exercise/:id' element={<ExerciseDetail/>} />
+      </Routes>
+    </Box>
+    </BrowserRouter>
   )
 }
