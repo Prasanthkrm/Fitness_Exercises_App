e945cd70c581248107fcb33e5ac1a92f0ac71f9a
PRASANTH M <prasanth.krm2003@gmail.com>
2025-03-30 20:19:12 +0530

    feat:add footer page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,24 +2,24 @@
 import React from 'react'
 import './App.css';
 import {Box} from '@mui/material';
-import {BrowserRouter, Route, Router, Routes} from 'react-router-dom';
+import { Route, Routes} from 'react-router-dom';
 import ExerciseDetail from './pages/ExerciseDetail';
 import Home from './pages/Home';
 import Navbar from './Components/Navbar';
+import { Footer } from './Components/Footer';
 
 
 export const App = () => {
   return (
-    // eslint-disable-next-line react/jsx-no-undef
     
-    <BrowserRouter>
-    <Box width="400px">
+    <Box width="400px" sx={{width:{xl:'1488px'}}} m="auto">
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/exercise/:id' element={<ExerciseDetail/>} />
       </Routes>
+      <Footer/>
     </Box>
-    </BrowserRouter>
+    
   )
 }
--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import Logo from '../assets/images/Logo.png';
+import { Stack} from '@mui/material';
+import {Link} from 'react-router-dom';
+
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+    <Stack>
+      <Link to="/"><img src={Logo} alt="logo" style={{width:'48px', height:'48px', margin:'0,20px'}}/></Link>
+      <Stack>
+        <Link to="/" style={{textDecoration:'none', color:'#3A1212'}}>Home</Link>
+        <a href="#exercies" style={{textDecoration:'none', color:'#3A1212'}}>Exercies</a>
+      </Stack>
+
+    </Stack>
+      
+    </div>
   )
 }
 
